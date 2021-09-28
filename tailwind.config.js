886726fb7c793f829d92c0b0a9e3379d75fe8d6c
Rahul Sharma <90137rahul@gmail.com>
2021-09-28 10:04:32 +0530

    :tada: Begin a project

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+// const colors = require("tailwindcss/colors");
+// module.exports = {
+//   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {},
+//     colors: {
+//       transparent: "transparent",
+//       current: "currentColor",
+//     },
+//   },
+//   variants: {
+//     extend: {},
+//   },
+// };
+
+module.exports = {
+  purge: ["./src/*/.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

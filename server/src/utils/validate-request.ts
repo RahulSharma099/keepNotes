2f0276c14c1464282dad04f035143d01941c46e0
Rahul Sharma <90137rahul@gmail.com>
2021-09-29 15:12:25 +0530

    🎨 Added validate request

---
+++ server/src/utils/validate-request.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from 'express';
+import * as yup from 'yup';
+
+type Location = 'query' | 'body' | 'params';
+
+// eslint-disable-next-line
+export function validateRequest(_schema: yup.ObjectSchema<any, any, any, any>, _location: Location) {
+  return async function isValid(req: Request, res: Response, next: NextFunction): Promise<void> {
+    let location: any;
+    try {
+      //🤔 using yup for validation
+      switch (_location) {
+        case 'body':
+          location = req.body;
+          break;
+        case 'params':
+          location = req.params;
+          break;
+        case 'query':
+          location = req.query;
+          break;
+      }
+      await _schema.validate(location, { abortEarly: false });
+      next();
+    } catch (err) {
+      let message = '';
+      err.errors.forEach((e: string) => {
+        message += `${e}. `;
+      });
+      next({ httpStatus: err.httpStatus || 400, message });
+    }
+  };
+}
